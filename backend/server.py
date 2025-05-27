6a648595380a64fdb2a0e641cc9a9ab4b87af029
IamRayen <rayenlabidi05042002@gmail.com>
2025-05-27 19:03:56 +0200

    DEVELOPMENT: Firebase integrated / Flask running

--- backend/server.py
+++ backend/server.py
@@ -1,10 +1,25 @@
 from flask import Flask
+import os
+
+import firebase_admin 
+from firebase_admin import credentials, initialize_app, firestore
+
+from dotenv import load_dotenv
+load_dotenv()  #Loads .env file
+cred_path = os.getenv("FIREBASE_CREDENTIALS_PATH") 
+
+
+#From firebase_admin
+cred = credentials.Certificate(cred_path)
+initialize_app(cred)
+db=firestore.client()
 
 app = Flask("Python-Server")
 
 # End-points:
 @app.route('/hello',methods=['GET']) 
 def function_hello():
+    db.collection('persons').add({"name":"John", "age": 40})
     return "Hello World"
 
 @app.route('/bye',methods=['GET'])
