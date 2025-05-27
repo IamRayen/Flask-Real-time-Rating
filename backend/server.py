69f714c5270c2b17b08868842e53b063ac91ccf0
IamRayen <rayenlabidi05042002@gmail.com>
2025-05-27 18:07:42 +0200

    Fixed small syntax in server.py

--- backend/server.py
+++ backend/server.py
@@ -3,14 +3,13 @@ from flask import Flask
 app = Flask("Python-Server")
 
 # End-points:
-@app.route('/hello',method=['GET'])
+@app.route('/hello',methods=['GET']) 
 def function_hello():
     return "Hello World"
 
-@app.route('/bye',method=['GET'])
+@app.route('/bye',methods=['GET'])
 def function_bye():
     return "Bye Bye"
-
 
 # Run the server
 app.run(port=3000)
