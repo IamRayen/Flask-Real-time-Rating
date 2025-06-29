b538e1fa5313e98c571a2e9efafeebee4cc7a375
Steffen Szramka <s.szramka@tu-braunschweig.de>
2025-06-29 09:28:36 +0000

    Edit __init__.py added CORS

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,9 +1,17 @@
 from flask import Flask
+from flask_cors import CORS
 
 def create_app():
     app = Flask(__name__)
+
+    CORS(app)
     
+    # handles events
     from app.routes.event_controller import event_bp
     app.register_blueprint(event_bp)
+
+    # handles questionnaires
+    from app.routes.questionnaire_controller import questionnaire_bp
+    app.register_blueprint(questionnaire_bp)
 
     return app
