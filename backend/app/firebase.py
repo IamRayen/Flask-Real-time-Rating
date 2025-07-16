ff647eb693423ee3cbd4b8b9f1ee11fc1535baec
IamRayen <rayenlabidi05042002@gmail.com>
2025-07-16 13:29:13 +0200

    fixed Firebase private key in firebase.py for the deployment

--- backend/app/firebase.py
+++ backend/app/firebase.py
@@ -3,7 +3,7 @@ from firebase_admin import credentials, initialize_app, firestore
 from dotenv import load_dotenv
 
 load_dotenv()
-cred_path = os.getenv("FIREBASE_CREDENTIALS_PATH")
-cred = credentials.Certificate(cred_path)
+#cred_path = os.getenv("FIREBASE_CREDENTIALS_PATH")
+cred = credentials.Certificate("/firebase-private-key.json")
 initialize_app(cred)
 db = firestore.client()
